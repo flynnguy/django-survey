409f9e9c1f3656ff15ff64df71397c6e05c52688
flynnguy <flynn@seesaw.net>
2010-02-17 18:28:24 -0500

    Added fixtures and a login test

--- survey/tests.py
+++ survey/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.test.client import Client
 from django.contrib.auth.models import User
-from survey.models import Question
+from survey.models import Survey, Question
+from datetime import datetime
 
 
 #TODO: Finish tests
@@ -16,4 +17,14 @@ class SimpleTest(TestCase):
     def test_editable(self):
         """We haven't logged in yet so this should be the login page"""
         response = self.client.get('/survey/editable/')
-        self.assertEqual(response.template[0].name, 'admin/login.html')
+        self.failUnlessEqual(response.template[0].name, 'admin/login.html')
+
+
+class SurveyCreation(TestCase):
+    fixtures = ['testdata',]
+
+    def test_login(self):
+        login = self.client.login(username='test', password='test')
+        self.failUnless(login, 'Could not log in')
+
+
